fe013018e9dba5904654b19ea991945c9fab6b14
Scott Simpson <sasimpson@gmail.com>
2016-10-18 15:18:44 -0500

    testing stuff

--- service/service.go
+++ service/service.go
@@ -111,13 +111,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTokenHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("getToken")
 	//pull apart incoming request
 	decoder := json.NewDecoder(r.Body)
 	var authRequest AuthRequest
 	err := decoder.Decode(&authRequest)
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "json decode failure", http.StatusBadRequest)
 		return
 	}
@@ -129,12 +127,12 @@ func getTokenHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid login", http.StatusUnauthorized)
 	}
 	//get the users token
-	token, err := dbi.getUserToken(user.ID)
+	token, err := dbi.getUserToken(user)
 	if err != nil {
 		log.Panicln(err)
 	}
-	log.Printf("%v", token)
-	log.Printf("%v", authRequest)
+	log.Printf("getTokenHandler token: %v", token)
+	log.Printf("getTokenHandler user: %v", user)
 	var authResponse AuthResponse
 	authResponse.Access.Token = token
 	authResponse.Access.User = user
@@ -143,10 +141,8 @@ func getTokenHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func validateTokenHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("validateToken")
-	vars := mux.Vars(r)
-	tokenID := vars["tokenID"]
-	log.Printf("tokenID: %v", tokenID)
+	// vars := mux.Vars(r)
+	// tokenID := vars["tokenID"]
 
 	var validateResponse AuthResponse
 	validateResponse.Access.User = User{Name: "userA", ID: 100}
@@ -174,22 +170,29 @@ func (db *db) close() {
 /* validateLogin - validates the user's login against the database
  */
 func (db *db) validateLogin(ar AuthRequest) (User, error) {
-	log.Println("validateLogin")
 	username := ar.Auth.PasswordCredentials.Username
 	password := ar.Auth.PasswordCredentials.Password
-	var user User
-	err := db.database.QueryRow("select id, name from users where name = ? and password = ?", username, password).Scan(&user.ID, &user.Name)
+	var userID int
+	err := db.database.QueryRow("select id from users where name = ? and password = ?", username, password).Scan(&userID)
+	log.Printf("validateLogin userID: %v", userID)
 	if err != nil {
-		return User{}, err
+		if err == sql.ErrNoRows {
+			return User{}, err
+		}
+		log.Panic(err)
+	}
+	user, err := db.User(userID)
+	log.Printf("validateLogin user: %v", user)
+	if err != nil {
+		log.Panic(err)
 	}
 	return user, nil
 }
 
 func (db *db) validateToken(tokenID string) bool {
 	var userID string
-	err := db.database.QueryRow("SELCT user_id from tokens where token_id = ?", tokenID).Scan(&userID)
+	err := db.database.QueryRow("select user_id from tokens where token_id = ?", tokenID).Scan(&userID)
 	if err != nil {
-		log.Println(err)
 		return false
 	}
 	return true
@@ -197,56 +200,100 @@ func (db *db) validateToken(tokenID string) bool {
 
 /* getUserToken - get the token in the DB for the user, generate one if there isn't one
  */
-func (db *db) getUserToken(userID int) (Token, error) {
+func (db *db) getUserToken(user User) (Token, error) {
 	var token Token
-	row := db.database.QueryRow("select token_id, expiration, created_at from tokens where user_id = ?", userID)
-	log.Printf("%v", row)
-	err := row.Scan(&token.ID, &token.Expires, &token.IssuedAt)
-	if err == sql.ErrNoRows {
-		token = db.newToken(userID)
-	} else {
-		if err != nil {
-			return Token{}, err
-		}
-	}
-
-	return token, nil
-}
-
-/* newToken: generates a new token and inserts it in the database for the user.
- */
-func (db *db) newToken(userID int) Token {
-	stmt, _ := db.database.Prepare("insert into tokens (token_id, user_id, created_at, expiration) values (?, ?, ?, ?)")
-	defer stmt.Close()
-	token := Token{
-		ID:       uuid.NewV4().String(),
-		IssuedAt: time.Now(),
-		Expires:  time.Now().AddDate(0, 0, 1)}
-	_, err := stmt.Exec(token.ID, userID, token.IssuedAt, token.Expires)
+	tokens, err := db.UserTokens(user)
 	if err != nil {
-		return Token{}
+		log.Panic(err)
 	}
-	return token
+	log.Printf("getUserToken tokens: %d %v", len(tokens), tokens)
+	if len(tokens) == 0 {
+		log.Printf("getUserToken user: %v", user)
+		token, err = db.newToken(user)
+		log.Printf("getUserToken token: %v", token)
+		if err != nil {
+			log.Panic(err)
+		}
+	} else {
+		token = tokens[0]
+	}
+	return token, nil
 }
 
 func (db *db) newAPIKey(userID int) (string, error) {
 	key := uuid.NewV4()
-	var apiKeyID int
-	db.database.QueryRow("select id from apikeys where user_id = ?", userID).Scan(&apiKeyID)
-	stmt, err := db.database.Prepare("delete from apikeys where user_id = ? and id = ?")
-	defer stmt.Close()
+	var keyCount int
+	err := db.database.QueryRow("select count(id) from apikeys where user_id = ?", userID).Scan(&keyCount)
+	if keyCount > 0 {
+		var apiKeyID int
+		err := db.database.QueryRow("select id from apikeys where user_id = ?", userID).Scan(&apiKeyID)
+		stmt, err := db.database.Prepare("delete from apikeys where user_id = ? and id = ?")
+		defer stmt.Close()
+		if err != nil {
+			return "", err
+		}
+		_, err = stmt.Exec(userID, apiKeyID)
+		if err != nil {
+			return "", err
+		}
+	}
+	stmt, err := db.database.Prepare("insert into apikeys (key, user_id, created_by, created_at) values (?, ?, ?, ?)")
 	if err != nil {
 		return "", err
 	}
-	_, err = stmt.Exec(userID, apiKeyID)
-	if err != nil {
-		return "", err
-	}
-	stmt, err = db.database.Prepare("insert into apikeys (key, user_id, created_by, created_at) values (?, ?, ?, ?)")
-	if err != nil {
-		return "", err
-	}
-	res, err := stmt.Exec(key.String(), userID, userID, time.Now())
-	log.Println(res.LastInsertId())
+	_, err = stmt.Exec(key.String(), userID, userID, time.Now())
 	return key.String(), nil
+}
+
+/* newToken: generates a new token and inserts it in the database for the user.
+ */
+func (db *db) newToken(user User) (Token, error) {
+	token := Token{
+		ID:       uuid.NewV4().String(),
+		IssuedAt: time.Now(),
+		Expires:  time.Now().AddDate(0, 0, 1)}
+	db.SetToken(token, user)
+	return token, nil
+}
+
+func (db *db) Token(id int) (Token, error) {
+	var token Token
+	err := db.database.QueryRow("select token_id, created_at, expiration from tokens where id = ?", id).Scan(&token.ID, &token.IssuedAt, &token.Expires)
+	return token, err
+}
+
+func (db *db) SetToken(token Token, user User) error {
+	stmt, err := db.database.Prepare("insert into tokens (token_id, user_id, created_at, expiration) values (?,?,?,?)")
+	_, err = stmt.Exec(token.ID, user.ID, token.IssuedAt, token.Expires)
+	stmt.Close()
+	return err
+}
+
+func (db *db) User(id int) (User, error) {
+	var user User
+	err := db.database.QueryRow("select id, name from users where id = ?", id).Scan(&user.ID, &user.Name)
+	log.Printf("db.User user: %v", user)
+	return user, err
+}
+
+func (db *db) UserTokens(user User) ([]Token, error) {
+	var tokens []Token
+	rows, err := db.database.Query("select id from tokens where user_id = ? order by expiration", user.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return []Token{}, nil
+		}
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var tokenID int
+		// log.Printf("db.UserTokens tokenID: %v", tokenID)
+		if err := rows.Scan(&tokenID); err != nil {
+			log.Fatal(err)
+		}
+		token, _ := db.Token(tokenID)
+		tokens = append(tokens, token)
+	}
+	return tokens, nil
 }
--- service/service_test.go
+++ service/service_test.go
@@ -4,74 +4,132 @@ import (
 	"testing"
 	"time"
 
+	"database/sql"
+
 	"github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-func TestValidateLogin(t *testing.T) {
+var testUser User
+var testToken Token
+
+func init() {
+	testUser = User{
+		ID:   1,
+		Name: "foo"}
+	testToken = Token{
+		ID:       uuid.NewV4().String(),
+		IssuedAt: time.Now(),
+		Expires:  time.Now().AddDate(0, 0, 1)}
+}
+
+func TestGetUserTokenExistingToken(t *testing.T) {
+	//setup
 	dba, mock, err := sqlmock.New()
 	assert := assert.New(t)
 	dbi.database = dba
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
+	assert.Nil(err, "opening database connection should work")
+	defer dbi.close()
+
+	mock.ExpectQuery("select id from tokens").WithArgs(1).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectQuery("select token_id, created_at, expiration from tokens").WithArgs(1).WillReturnRows(sqlmock.NewRows([]string{"token_id", "created_at", "expiration"}).AddRow(testToken.ID, testToken.IssuedAt, testToken.Expires))
+
+	token, err := dbi.getUserToken(testUser)
+	assert.Equal(testToken.ID, token.ID)
+	err = mock.ExpectationsWereMet()
+	assert.Empty(err)
+}
+
+func TestGetUserTokenNoTokens(t *testing.T) {
+	//setup
+	dba, mock, err := sqlmock.New()
+	assert := assert.New(t)
+	dbi.database = dba
+	assert.Nil(err, "opening database connection should work")
+	defer dbi.close()
+
+	mock.ExpectQuery("select id from tokens").WithArgs(1).WillReturnError(sql.ErrNoRows)
+	mock.ExpectPrepare("insert into tokens").ExpectExec().WillReturnResult(sqlmock.NewResult(1, 1))
+	_, err = dbi.getUserToken(testUser)
+	err = mock.ExpectationsWereMet()
+	assert.Empty(err)
+}
+
+func TestValidateLoginValid(t *testing.T) {
+	//setup
+	dba, mock, err := sqlmock.New()
+	assert := assert.New(t)
+	dbi.database = dba
+	assert.Nil(err, "opening database connection should work")
 	defer dbi.database.Close()
-	mock.ExpectQuery("select id, name from users").
+	//test valid login for foo user
+	mock.ExpectQuery("select id from users").
 		WithArgs("foo", "bar").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectQuery("select id, name from users").
+		WithArgs(1).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "foo"))
+
 	var ar AuthRequest
 	ar.Auth.PasswordCredentials.Username = "foo"
 	ar.Auth.PasswordCredentials.Password = "bar"
 	user, err := dbi.validateLogin(ar)
+	err = mock.ExpectationsWereMet()
+	assert.Empty(err)
 	assert.Equal(user.Name, ar.Auth.PasswordCredentials.Username)
 	assert.Equal(user.ID, 1)
-	mock.ExpectQuery("select id, name from users").
+}
+
+func TestValidateLoginNotValid(t *testing.T) {
+	//setup
+	dba, mock, err := sqlmock.New()
+	assert := assert.New(t)
+	dbi.database = dba
+	assert.Nil(err, "opening database connection should work")
+	defer dbi.database.Close()
+	mock.ExpectQuery("select id from users").
 		WithArgs("foo", "bar").
-		WillReturnRows(sqlmock.NewRows([]string{}).AddRow())
-	user, err = dbi.validateLogin(ar)
-	if err != nil {
-		t.Logf("%v", err)
-	}
+		WillReturnError(sql.ErrNoRows)
+	var ar AuthRequest
+	ar.Auth.PasswordCredentials.Username = "foo"
+	ar.Auth.PasswordCredentials.Password = "bar"
+	_, err = dbi.validateLogin(ar)
+	assert.Error(err, "sql: no rows in result set")
+	err = mock.ExpectationsWereMet()
+	assert.Empty(err)
 }
 
 func TestNewToken(t *testing.T) {
 	dba, mock, err := sqlmock.New()
 	assert := assert.New(t)
 	dbi.database = dba
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
+	assert.Nil(err, "opening database connection should work")
 	defer dbi.close()
-	testToken := Token{
-		ID:       uuid.NewV4().String(),
-		IssuedAt: time.Now(),
-		Expires:  time.Now().AddDate(0, 0, 1)}
-	mock.ExpectPrepare("insert into tokens (token_id, user_id, created_at, expiration)").
+
+	// test getting a new token generated and inserted into the db
+	mock.ExpectPrepare("insert into tokens").
 		ExpectExec().
-		WithArgs(testToken.ID, 1, testToken.IssuedAt, testToken.Expires)
-	token := dbi.newToken(1)
-	assert.Equal(Token{}.ID, token.ID)
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	token, err := dbi.newToken(testUser)
+	assert.Nil(err)
+	assert.NotEmpty(token)
+	err = mock.ExpectationsWereMet()
+	assert.Empty(err)
 }
 
-func TestGetUserToken(t *testing.T) {
+/*
+func TestNewAPIKey(t *testing.T) {
+	t.Skip()
 	dba, mock, err := sqlmock.New()
 	assert := assert.New(t)
 	dbi.database = dba
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
+	assert.Nil(err, "opening database connection should work")
 	defer dbi.close()
-	testTime := time.Date(2016, time.January, 1, 0, 0, 0, 0, time.UTC)
-	mock.ExpectQuery("select token_id, expiration, created_at from tokens").
+	mock.ExpectQuery("select count(id) from apikeys where user_id = ?").
 		WithArgs(1).
-		WillReturnRows(sqlmock.NewRows([]string{"token_id", "expiration", "created_at"}).AddRow("1", testTime, testTime.AddDate(0, 0, 1)))
-	token, err := dbi.getUserToken(1)
-	assert.Equal("1", token.ID)
-
-	mock.ExpectQuery("select token_id, expiration, created_at from tokens").
-		WithArgs(1).
-		WillReturnRows(sqlmock.NewRows([]string{}).AddRow())
-	token, err = dbi.getUserToken(1)
-	assert.Equal(Token{}, token)
+		WillReturnRows(sqlmock.NewRows([]string{"count(id)"}).AddRow(1))
+	_, err = dbi.newAPIKey(1)
+	err = mock.ExpectationsWereMet()
 }
+*/
